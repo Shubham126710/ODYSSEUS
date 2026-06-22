629968a181150c32c32579376cc7dc6ed79c8d2b
Shubham Upadhyay <109911788+Shubham126710@users.noreply.github.com>
2026-06-22 16:01:48 +0530

    chore: ignore eslint and ts errors on vercel build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['jsdom', '@mozilla/readability'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
